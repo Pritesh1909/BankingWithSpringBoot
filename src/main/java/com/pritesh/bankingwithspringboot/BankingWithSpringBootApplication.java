eda17aab828e73d585175b4c132bc1fd3c4cb675
pritesh <pritesh.190410116146@svitvasad.ac.in>
2022-05-15 22:08:16 +0530

    Initial commit

---
+++ src/main/java/com/pritesh/bankingwithspringboot/BankingWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.pritesh.bankingwithspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingWithSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pritesh/bankingwithspringboot/BankingWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pritesh.bankingwithspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingWithSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
